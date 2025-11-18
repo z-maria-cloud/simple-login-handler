19c7a4c57799e3d0457652946954fcdcfeeb1e51
z-maria-cloud <197367564+z-maria-cloud@users.noreply.github.com>
2025-11-18 11:02:03 +0100

    register users to database

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
-const { scryptSync } = await import('node:crypto');
+const { scryptSync } = await import("node:crypto");
 
 // bcrypt init + basic test
 
 import bcrypt from "bcrypt";
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
 
 const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-console.log(hash)
+console.log(hash);
 console.log(bcrypt.compareSync(myPlaintextPassword, hash)); // true
 console.log(bcrypt.compareSync(someOtherPlaintextPassword, hash)); // false
 
@@ -20,41 +20,45 @@ const app = express();
 const port = process.env.PROJECT_PORT;
 const appName = process.env.PROJECT_NAME;
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // db connection init
 
-import { Client } from 'pg'
+import { Client } from "pg";
 const db = new Client({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 try {
-  await db.connect()
-} catch(error) {
-  console.log(error)
-  console.log("could not connect to database.")
+  await db.connect();
+} catch (error) {
+  console.log(error);
+  console.log("could not connect to database.");
 }
 
 // simple dbquery function
 
 async function dbQuery(query, data) {
-    const result = await db.query(query, data)
-    return result.rows
+  const result = await db.query(query, data);
+  return result.rows;
 }
 
-let aaa = await dbQuery("SELECT * FROM data;", [])
-console.log(aaa)
-
 app.get("/", (req, res) => {
-  res.render("register.ejs")
+  res.render("register.ejs");
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body)
-  res.redirect("/")
+app.post("/register", async (req, res) => {
+  // hash password, then store hash + user data to database
+  const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+  let storeUser = await dbQuery(
+    "INSERT INTO data (name, username, password) VALUES ($1, $2, $3)",
+    [req.body.name, req.body.username, hash]
+  );
+  res.send(
+    "<p>Registration was successful. Try logging in now!</p><a href='/'>Go back to Homepage</a>"
+  );
 });
 
 app.listen(port, () => {
