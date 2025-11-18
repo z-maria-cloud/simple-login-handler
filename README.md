e98cd53fbcf2784530220c4ac145a2accb53c2cf
z-maria-cloud <197367564+z-maria-cloud@users.noreply.github.com>
2025-11-18 10:29:13 +0100

    added post route, bcrypt init

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Login Handler
 
 Lets an user create an account, log in and log out.
+
+## Important notes
+
+This project was created in order to showcase how password handling works **FOR LEARNING PURPOSES ONLY**. It's **essential** to use **secure tools like OAUTH** instead of writing login handlers from scratch.
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import "dotenv/config";
+const { scryptSync } = await import('node:crypto');
+
+// bcrypt init + basic test
+
+import bcrypt from "bcrypt";
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+console.log(bcrypt.compareSync(myPlaintextPassword, hash)); // true
+console.log(bcrypt.compareSync(someOtherPlaintextPassword, hash)); // false
+
+// express init
 
 import express from "express";
 const app = express();
 const port = process.env.PROJECT_PORT;
 const appName = process.env.PROJECT_NAME;
+
+app.use(express.urlencoded({ extended: true }))
+
+// db connection init
 
 import { Client } from 'pg'
 const db = new Client({
@@ -11,18 +30,31 @@ const db = new Client({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
-await db.connect()
 
-async function dbTransaction(query, data) {
+try {
+  await db.connect()
+} catch(error) {
+  console.log(error)
+  console.log("could not connect to database.")
+}
+
+// simple dbquery function
+
+async function dbQuery(query, data) {
     const result = await db.query(query, data)
     return result.rows
 }
 
-let aaa = await dbTransaction("SELECT * FROM data;", [])
+let aaa = await dbQuery("SELECT * FROM data;", [])
 console.log(aaa)
 
 app.get("/", (req, res) => {
-  res.send("hello world")
+  res.render("register.ejs")
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body)
+  res.redirect("/")
 });
 
 app.listen(port, () => {
