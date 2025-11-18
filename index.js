f0b7237fcbd07e75be50ad56eb32e774bdf20a2b
z-maria-cloud <197367564+z-maria-cloud@users.noreply.github.com>
2025-11-18 14:35:44 +0100

    successful logins are added to active sessions

--- index.js
+++ index.js
@@ -30,18 +30,21 @@ app.use(express.urlencoded({ extended: true }));
 // custom middleware that handles cookie / sessions logic
 
 app.use((req, res, next) => {
-  if (Object.keys(req.cookies).length == 0) {
-    // if as no cookie then give a new one immediately
+  if (Object.keys(req.cookies).length == 0 || !Object.keys(activeSessions).includes(req.cookies.id)) {
+    // if:
+    // - has no cookie
+    // - has a cookie that's not listed in active sessions
+    // then give a new one immediately
     // also add newly created cookie to the active sessions object with status of "no login"
     let newCookie = cookieString()
-    console.log(`no cookies have been found, delivering one (${newCookie}) rn`)
-    activeSessions[newCookie] = false
+    console.log(`no valid cookies have been found, delivering one (${newCookie}) right now. creating new session`)
+    activeSessions[newCookie] = {loggedIn: false, userName: null}
     res.cookie("id", newCookie)
   } else {
     // already has an id cookie, so dont give out a new one
     console.log(req.cookies)
   }
-  console.log(activeSessions)
+  console.log("activeSessions:",activeSessions)
   next()
 })
 
@@ -72,9 +75,12 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("/deletecookies", (req, res) => {
+app.get("/logout", (req, res) => {
+  let currentCookie = req.cookies.id
+  delete activeSessions[currentCookie]
   res.clearCookie("id");
-  res.send("id cookie was deleted")
+  console.log(`deleted active session for cookie: ${currentCookie}`)
+  res.send("<p>Successfully logged out.</p><a href='/'>Go back to Homepage</a>")
 });
 
 app.get("/new-user", (req, res) => {
@@ -125,6 +131,7 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const userName = escape(req.body.username);
   const password = req.body.password;
+  const currentCookie = req.cookies.id
   let sendString = "<p>Username or password was incorrect. Please try again.</p><a href='/'>Go back to Homepage</a>";
 
   // check if password is correct for given username
@@ -136,8 +143,12 @@ app.post("/login", async (req, res) => {
   // if checkPassword is empty, then no user with that username exists.
 
   if (checkPassword.length > 0) {
-    if (bcrypt.compareSync(req.body.password, checkPassword[0].password)) {
+    if (bcrypt.compareSync(password, checkPassword[0].password)) {
+      // if login is successful, then we have to assign "true"
+      // to the current request's cookie's login status in the active sessions object
+      // and set the correct username.
       sendString = "<p>Login was successful!</p><a href='/'>Go back to Homepage</a>";
+      activeSessions[currentCookie] = {loggedIn: true, userName: userName}
     }
   }
 
